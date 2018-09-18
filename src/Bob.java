63b2793a70d42b4a334e2ab45e0613f640c608b8
tcolejimenez <taylor.cole.jimenez@gmail.com>
2018-09-18 14:36:35 -0500

    Day 42 - completed String exercises

--- src/Bob.java
+++ src/Bob.java
@@ -1,20 +1,36 @@
+import java.util.Scanner;
+
 public class Bob {
     //Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+    //Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
 
     public static void main(String[] args) {
 
-        //Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Let's talk to Bob! (enter end to exit)");
 
-        //He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+        String input = "";
+        while (!input.equals("end")) {
+            System.out.print("You: ");
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine();
 
+            if (!input.equals("end")) {
+                //He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+                if (input.isEmpty()) System.out.println("Bob: Fine. Be that way!");
 
-        //He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+                    //He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+                else if (input.trim().charAt(input.trim().length() - 1) == '!') System.out.println("Bob: Whoa, chill out!");
 
+                    //Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+                else if (input.trim().charAt(input.trim().length() - 1) == '?') System.out.println("Bob: Sure.");
 
-        //He answers 'Whatever.' to anything else.
+                    //He answers 'Whatever.' to anything else.
+                else System.out.println("Bob: Whatever.");
 
+            }
+        }
 
-        //Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
     }
 }
