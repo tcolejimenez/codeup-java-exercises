b0ff8d389e54bded11707dd9c88bb60f4d02f98f
tcolejimenez <taylor.cole.jimenez@gmail.com>
2018-09-18 11:54:15 -0500

    Working on Control Flow Exercises, on last problem. table for square/cube works correctly

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,4 +1,129 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ConsoleExercises {
+
+    //executing method
+    public static void main(String[] args) {
+        double pi = 3.14159;
+
+        /*
+        * Write some java code that uses the variable pi to output the following:
+        * "The value of pi is approximately 3.14."
+        * Don't change the value of the variable, instead,
+        * reference one of the links above and use System.out.format to accomplish this.
+        */
+
+        System.out.format("The value of pi is approximately %.2f. %n", pi);
+
+        /*
+        * Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
+        * What happens if you input something that is not an integer?
+        */
+
+        String prompt = "Please enter an integer:";
+        Scanner scanner = new Scanner(System.in);
+
+        //System.out.println(prompt);
+        //int userInput = scanner.nextInt();
+        //System.out.println("You entered >>>>" + userInput +"<<<<<!");
+
+        /*
+        * Prompt a user to enter 3 words and store each of them in a separate variable, then display them back, each on a newline.
+        * What happens if you enter less than 3 words?
+        * What happens if you enter more than 3 words?
+        */
+
+        prompt = "Please enter 3 words:";
+        System.out.println(prompt);
+        String word1 = scanner.next();
+        String word2 = scanner.next();
+        String word3 = scanner.next();
+
+        System.out.format("You entered: %n %s %n %s %n %s %n", word1, word2, word3);
+
+        /*
+        * Prompt a user to enter a sentence, then store that sentence in a String variable using the .next method,
+        * then display that sentence back to the user.
+        * do you capture all of the words?
+        */
+
+//        prompt = "Please enter a sentence:";
+//        System.out.println(prompt);
+//        String sentence = scanner.next();
+//
+//        System.out.println("Looks like the sentence you entered was: \n" + sentence);
+
+        /*
+        Rewrite the above example using the .nextLine method.
+        */
+
+        prompt = "Please enter a sentence:";
+        //System.out.println(prompt);
+        //String sentence2 = scanner.nextLine();
+
+        //System.out.println("Looks like the sentence you entered was: \n" + sentence2);
+
+        /*
+        * Calculate the perimeter and area of Codeup's classroom.
+        * Prompt the user to enter values of length and width of a classroom at Codeup
+        * Use the .nextLine method to get user input and cast the resulting string to a numeric type.
+        * Assume that the rooms are perfect rectangles.
+        * Assume that the user will enter valid numeric data for length and width.
+        *
+        * Display the area and perimeter of that classroom.
+        * The area of a rectangle is equal to the length times the width,
+        * and the perimeter of a rectangle is equal to 2 times the length plus 2 times the width.
+        */
+
+
+//        prompt = "Please enter the length and width separated with a space:";
+//        System.out.println(prompt);
+//        int length = Integer.parseInt(scanner.next());
+//        int width = Integer.parseInt(scanner.next());
+//
+//        int perimeter = (length * 2) + (width * 2);
+//        int area = length * width;
+//
+//        System.out.println("Based on the input, the area is: " + area + ". ");
+//        System.out.println("Based on the input, the perimeter is: " + perimeter + ". ");
+
+
+        /*
+        * Bonuses
+        * Accept decimal entries
+        * Calculate the volume of the rooms in addition to the area and perimeter
+        */
+
+        char moreRooms = 'y';
+        double totalVolume = 0;
+        while (moreRooms =='y') {
+            prompt = "Please enter the length and width, separated with a space: ";
+            System.out.println(prompt);
+
+            double length = scanner.nextDouble();
+            double width = scanner.nextDouble();
+
+            totalVolume += printArea(length, width);
+            printPerimeter(length, width);
+
+            prompt = "Are you any more rooms?";
+            System.out.println(prompt);
+            moreRooms = scanner.next().charAt(0);
+        }
+
+        System.out.println("The total volume of classroom(s) is "+ totalVolume + ". ");
+
+
+    }
+
+    public static double printArea(double length, double width) {
+        double area = length * width;
+        System.out.println("Based on the input, the area is: " + area + ". ");
+        return area;
+    }
+
+    public static void printPerimeter(double length, double width) {
+        double perimeter = length * 2 + width * 2;
+        System.out.println("Based on the input, the perimeter is: " + perimeter + ". ");
+    }
 }
--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,4 +1,195 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ControlFlowExercises {
+
+    public static void main (String[] args) {
+        //Loop Basics
+
+        /* While
+        * Create an integer variable i with a value of 5.
+        * Create a while loop that runs so long as i is less than or equal to 15
+        * Each loop iteration, output the current value of i, then increment i by one.
+        * Your output should look like this:
+        * 5 6 7 8 9 10 11 12 13 14 15
+        */
+
+        System.out.println("Print 5 - 15 on one line");
+        int i = 5;
+        while (i <= 15) {
+            System.out.print(i++ + " ");
+        }
+        System.out.print("\n");
+
+        /* Do While
+        * Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+        * Alter your loop to count backwards by 5's from 100 to -10.
+        * Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000.
+        * Output should equal:
+        * 2
+        * 4
+        * 16
+        * 256
+        * 65536
+        * */
+
+
+        System.out.println("Add two while less than or equal to 100:");
+        i = 0;
+        do {
+            System.out.println(i);
+            i += 2;
+        } while (i <= 100);
+        System.out.print("\n");
+
+
+        System.out.println("Subtract 5 while greater than or equal to -10:");
+        i = 100;
+        do {
+            System.out.println(i);
+            i -= 5;
+        } while (i >= -10);
+        System.out.print("\n");
+
+
+        System.out.println("Square while under 1000000:");
+        i = 2;
+        do {
+           System.out.println(i);
+           i = (int) Math.pow(i, 2);
+        } while (i < 1000000);
+        System.out.print("\n");
+
+
+        /* For loop
+        * refactor the previous two exercises to use a for loop instead
+        * */
+        System.out.println("Using for loop, print 5-15:");
+        for (int z = 5; z <= 15; z++) {
+            System.out.print(z + " ");
+        }
+        System.out.print("\n\n");
+
+        System.out.println("Using for loop, add two while less than or equal to 100:");
+        for (int z = 0; z <= 100; z+=2) {
+            System.out.println(z);
+        }
+        System.out.print("\n");
+
+        System.out.println("Using for loop, subtract 5 while greater than or equal to -10:");
+        for (int z = 100; z >= -10; z-=5) {
+            System.out.println(z);
+        }
+        System.out.print("\n");
+
+        /* Fizzbuzz
+        *  Write a program that prints the numbers from 1 to 100.
+        *  For multiples of three print “Fizz” instead of the number
+        *  For the multiples of five print “Buzz”.
+        *  For numbers which are multiples of both three and five print “FizzBuzz”.
+        */
+
+        System.out.println("Fizzbuzz using for loop");
+        for (int z = 1; z <= 100; z++) {
+            if (z % 3 == 0 || z % 5 == 0) {
+                if (z % 3 == 0) System.out.print("Fizz");
+                if (z % 5 == 0) System.out.print("Buzz");
+            }
+            else {
+                System.out.print(z);
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+
+
+        /* Display a table of powers.
+        *  Prompt the user to enter an integer.
+        *  Display a table of squares and cubes from 1 to the value entered.
+        *  Ask if the user wants to continue.
+        *  Assume that the user will enter valid data.
+        *  Only continue if the user agrees to.
+        *
+        *  What number would you like to go up to? 5
+
+        * Here is your table!
+
+        number | squared | cubed
+        ------ | ------- | -----
+        1      | 1       | 1
+        2      | 4       | 8
+        3      | 9       | 27
+        4      | 16      | 64
+        5      | 25      | 125
+        */
+
+        //initializes char for loop
+        //while continue as long as value will be y
+        //prompts user for new value at end of table
+        char cont = 'y';
+        while (cont == 'y') {
+
+            //get number to count up to
+            System.out.println("Please enter an integer:");
+            Scanner scanner = new Scanner(System.in);
+            int num = scanner.nextInt();
+
+            //adds header for table
+            System.out.println("number | squared | cubed \n------ | ------- | -----");
+
+            //loops based to print for every value up to number entered
+            for (int z = 1; z <= num; z++) {
+
+                //calculates squared number and cubed
+                int sqrd = z * z;
+                int cubed = (int) Math.pow(z, 3);
+
+                //calculate number of spaces needed based on length of number
+                int numSpaces = 6 - String.valueOf(z).length();
+                int sqrdSpaces = 7 - String.valueOf(sqrd).length();
+                //int cubeSpaces = 7 - String.valueOf(cube).length();
+
+                //adds number to output and required number of spaces
+                String output = String.valueOf(z);
+                for (int y = 0; y <= numSpaces; y++) output += " ";
+
+                //adds squared number and required number of spaces
+                output += String.valueOf("| " + sqrd);
+                for (int y = 0; y <= sqrdSpaces; y++) output += " ";
+
+                //simply adds cubed number as spaces aren't needed for formatting
+                output += String.valueOf("| " + cubed);
+
+                //print out formatted output
+                System.out.println(output);
+            }
+
+            //ask user if they'd like to continue
+            System.out.println("\nWould you like to try another number? (y/n)");
+            String response = scanner.next();
+            cont = response.charAt(0);
+        }
+
+        /* Convert given number grades into letter grades.
+        *  Prompt the user for a numerical grade from 0 to 100.
+        *  Display the corresponding letter grade.
+        *  Prompt the user to continue.
+        *  Assume that the user will enter valid integers for the grades.
+        *  The application should only continue if the user agrees to.
+        *  Grade Ranges: A : 100 - 88, B : 87 - 80, C : 79 - 67, D : 66 - 60, F : 59 - 0
+        * */
+
+
+
+
+
+        /* Bonus
+        *  Edit your grade ranges to include pluses and minuses (ex: 99-100 = A+).
+        */
+
+
+
+
+
+    }
+
 }
